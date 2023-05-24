7b5815fd8af25e6e53a8889aa6d2d3f9ca6a6e43
FelipeSousa@ <103294274+FelipeSousa2@users.noreply.github.com>
2023-05-23 21:32:45 -0300

    projeto completed

--- js/login.js
+++ js/login.js
@@ -7,3 +7,7 @@ const moveOverlay = () => LoginContainer.classList.toggle('move')
 document.getElementById('open-register').addEventListener('click', moveOverlay);
 
 document.getElementById('open-login').addEventListener('click', moveOverlay);
+
+document.getElementById('open-register-mobile').addEventListener('click', moveOverlay);
+
+document.getElementById('open-login-mobile').addEventListener('click', moveOverlay);
